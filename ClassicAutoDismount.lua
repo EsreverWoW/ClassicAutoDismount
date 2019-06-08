9edc837f6ff70a3d751eaa0150198c27869af96d
Esrever <49285632+EsreverWoW@users.noreply.github.com>
2019-06-08 16:05:02 -0500

    1.0

---
+++ ClassicAutoDismount.lua
@@ -0,0 +1,22 @@
+﻿----------------------------------------------------------------------------------------
+--	ClassicAutoDismount (by EsreverWoW)
+----------------------------------------------------------------------------------------
+local frame = CreateFrame("Frame")
+
+local errorMessages = {
+	[ERR_ATTACK_MOUNTED]			= true,
+	[ERR_MOUNT_ALREADYMOUNTED]		= true,
+	[ERR_NOT_WHILE_MOUNTED]			= true,
+	[ERR_TAXIPLAYERALREADYMOUNTED]	= true,
+	[SPELL_FAILED_NOT_MOUNTED]		= true,
+}
+
+function CheckDismount(self, event, ...)
+	if not IsMounted() or not errorMessages[select(2, ...)] then return end
+
+	Dismount()
+	UIErrorsFrame:Clear()
+end
+
+frame:RegisterEvent("UI_ERROR_MESSAGE")
+frame:SetScript("OnEvent", CheckDismount)
